72b990c1c703616ccb942c4560806c62b5253411
Benjamin <benjamin.kinnard1@gmail.com>
2017-06-15 17:20:25 +0100

    added an image

--- mockterview/react/client/src/components/Pitch.jsx
+++ mockterview/react/client/src/components/Pitch.jsx
@@ -9,8 +9,9 @@ class Pitch extends React.Component{
       <div className="mockterviewPitch">
       <p>Want to get better at interviewing?</p>
       <p>Want to know how you really did?</p>
-      <p>Schedule a mock interview with a real HR professional</p>
+      <p>Schedule a mock interview with a real HR professional.</p>
       <p>Get some real feedback. Get better. Get a job.</p>
+      <img src="../../images/get_a_job.jpg" alt="Oh just get a job" height="200" width="320"/>
       </div>
     )
   }
